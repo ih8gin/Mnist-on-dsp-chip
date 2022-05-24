ffbdae9a9b28b3e14fdcacdf11c5ed6244667526
ih8gin <91772727+ih8gin@users.noreply.github.com>
2022-05-24 15:20:47 +0800

    c file loaded to C6416
    Using fully CNN with 2 5*5 convolution layers with max pooling, 1 average pooling and 1 1*1 convolution layer. 
    Size of input image is [28, 28]

---
+++ main.c
@@ -0,0 +1,317 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define MULTI 1000      // float*1000 -> int
+#define COUNT_OF_DATA 500
+
+#pragma DATA_SECTION (dataset, ".array_buf");
+#pragma DATA_ALIGN (dataset, 16);
+#pragma DATA_SECTION (labels, ".array_buf");
+#pragma DATA_ALIGN (labels, 16);
+#pragma DATA_SECTION (conv1_w, ".array_buf");
+#pragma DATA_ALIGN (conv1_w, 16);
+#pragma DATA_SECTION (conv1_b, ".array_buf");
+#pragma DATA_ALIGN (conv1_b, 16);
+#pragma DATA_SECTION (conv2_w, ".array_buf");
+#pragma DATA_ALIGN (conv2_w, 16);
+#pragma DATA_SECTION (conv2_b, ".array_buf");
+#pragma DATA_ALIGN (conv2_b, 16);
+#pragma DATA_SECTION (conv3_w, ".array_buf");
+#pragma DATA_ALIGN (conv3_w, 16);
+#pragma DATA_SECTION (conv3_b, ".array_buf");
+#pragma DATA_ALIGN (conv3_b, 16);
+#pragma DATA_SECTION (input, ".array_buf");
+#pragma DATA_ALIGN (input, 16);
+#pragma DATA_SECTION (layer1_conv, ".array_buf");
+#pragma DATA_ALIGN (layer1_conv, 16);
+#pragma DATA_SECTION (layer1_pool, ".array_buf");
+#pragma DATA_ALIGN (layer1_pool, 16);
+#pragma DATA_SECTION (layer2_conv, ".array_buf");
+#pragma DATA_ALIGN (layer2_conv, 16);
+#pragma DATA_SECTION (layer2_pool, ".array_buf");
+#pragma DATA_ALIGN (layer2_pool, 16);
+#pragma DATA_SECTION (layer3_conv, ".array_buf");
+#pragma DATA_ALIGN (layer3_conv, 16);
+#pragma DATA_SECTION (layer3_pool, ".array_buf");
+#pragma DATA_ALIGN (layer3_pool, 16);
+#pragma DATA_SECTION (output, ".array_buf");
+#pragma DATA_ALIGN (output, 16);
+
+int dataset[COUNT_OF_DATA][28][28];
+int labels[COUNT_OF_DATA];
+int conv1_w[5][5][5];
+int conv1_b[5];
+int conv2_w[10][5][5];
+int conv2_b[10];
+int conv3_w[10];
+int conv3_b[10];
+long input[28][28];
+long layer1_conv[5][24][24];
+long layer1_pool[5][12][12];
+long layer2_conv[10][8][8];
+long layer2_pool[10][4][4];
+long layer3_pool[10];
+long layer3_conv[10];
+int output;
+
+
+void conv1() { // [1,28,28] * [5,5,5] -> [5,24,24]
+    int c, h, w, x, y;
+    for (c = 0; c < 5; c++) {
+        for (h = 0; h < 24; h++) {
+            for (w = 0; w < 24; w++) {
+                //compute convlution result of one output pixel
+                for (x = 0; x < 5; x++) {
+                    for (y = 0; y < 5; y++) {
+                        layer1_conv[c][h][w] += input[h + x][w + y] * conv1_w[c][x][y] + conv1_b[c];
+                    }
+                }
+                //relu
+                if (layer1_conv[c][h][w] < 0)layer1_conv[c][h][w] = 0;
+            }
+        }
+    }
+}
+
+void pooling1() { // [5,24,24] -> [5,12,12]
+    int c, h, w, x, y;
+    for (c = 0; c < 5; c++) {
+        for (h = 0; h < 12; h++) {
+            for (w = 0; w < 12; w++) {
+                //compute max pooling result of one output pixel
+                for (x = 0; x < 2; x++) {
+                    for (y = 0; y < 2; y++) {
+                        if (layer1_pool[c][h][w] < layer1_conv[c][h + x][w + y]) {
+                            layer1_pool[c][h][w] = layer1_conv[c][h + x][w + y];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void conv2() { // [5,12,12] * [10,5,5] -> [10,8,8]
+    int c, h, w, x, y, z;
+    for (c = 0; c < 10; c++) {
+        for (h = 0; h < 8; h++) {
+            for (w = 0; w < 8; w++) {
+                //compute convlution result of one output pixel
+                for (x = 0; x < 5; x++) {
+                    for (y = 0; y < 5; y++) {
+                        for (z = 0; z < 5; z++) {
+                            layer2_conv[c][h][w] += layer1_pool[h + x][w + y][z] * conv2_w[c][x][y] + conv2_b[c];
+                        }
+                    }
+                }
+                //relu
+                if (layer2_conv[c][h][w] < 0)layer2_conv[c][h][w] = 0;
+            }
+        }
+    }
+}
+
+void pooling2() { // [10,8,8] -> [10,4,4]
+    int c, h, w, x, y;
+    for (c = 0; c < 10; c++) {
+        for (h = 0; h < 4; h++) {
+            for (w = 0; w < 4; w++) {
+                //compute max pooling result of one output pixel
+                for (x = 0; x < 2; x++) {
+                    for (y = 0; y < 2; y++) {
+                        if (layer2_pool[c][h][w] < layer2_conv[c][h + x][w + y]) {
+                            layer2_pool[c][h][w] = layer2_conv[c][h + x][w + y];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void pooling3() { // [10,4,4] -> [10,1,1]
+    int c, x, y;
+    for (c = 0; c < 10; c++) {
+        //compute average pooling result of one output pixel
+        for (x = 0; x < 4; x++) {
+            for (y = 0; y < 4; y++) {
+                layer3_pool[c] += layer2_pool[c][x][y] / 4;
+            }
+        }
+    }
+}
+
+void conv3() { // [10,1,1] * [10,1,1] -> [10,1,1]
+    int c, z;
+    for (c = 0; c < 10; c++) {
+        //compute convlution result of one output pixel
+        for (z = 0; z < 10; z++) {
+            layer3_conv[c] += layer3_pool[z] * conv3_w[c] + conv3_b[c];
+        }
+        //no need of softmax activation
+    }
+}
+
+void model_run() {
+    int i;
+    conv1();
+    pooling1();
+    conv2();
+    pooling2();
+    pooling3();
+    conv3();
+    for (i = 0; i < 10; i++) {
+        if (layer3_conv[i] > layer3_conv[output]) {
+            output = i;
+        }
+    }
+}
+
+void get_weights() { // 'CHW'format
+
+    FILE * fp;
+    int c, h, w;
+    if (NULL == (fp = fopen("..\\data\\conv1_w.txt", "r"))) {
+        printf("conv1_w open failed\n");
+        exit(1);
+    }
+    printf("conv1_w open succeed\n");
+    // conv1.shape = [5,5,5]
+    for (c = 0; c < 5; ++c) {
+        for (h = 0; h < 5; ++h) {
+            for (w = 0; w < 5; ++w) {
+                fscanf(fp, "%d", &conv1_w[c][h][w]);
+            }
+        }
+    }
+    printf("conv1_w load succeed\n");
+    fclose(fp);
+
+    if (NULL == (fp = fopen("..\\data\\conv1_b.txt", "r"))) {
+        printf("conv1_b open failed\n");
+        exit(1);
+    }
+    printf("conv1_b open succeed\n");
+    // conv1.b.shape = [5,1]
+    for (c = 0; c < 5; ++c) {
+        fscanf(fp, "%d", &conv1_b[c]);
+    }
+    printf("conv1_b load succeed\n");
+    fclose(fp);
+
+
+    if (NULL == (fp = fopen("..\\data\\conv2_w.txt", "r"))) {
+        printf("conv2_w open failed\n");
+        exit(1);
+    }
+    printf("conv2_w open succeed\n");
+    // conv2.shape = [10,5,5]
+    for (c = 0; c < 10; ++c) {
+        for (h = 0; h < 5; ++h) {
+            for (w = 0; w < 5; ++w) {
+                fscanf(fp, "%d", &conv2_w[c][h][w]);
+            }
+        }
+    }
+    printf("conv2_w load succeed\n");
+    fclose(fp);
+
+    if (NULL == (fp = fopen("..\\data\\conv2_b.txt", "r"))) {
+        printf("conv2_b open failed\n");
+        exit(1);
+    }
+    printf("conv2_b open succeed\n");
+    // conv2.b.shape = [10,1]
+    for (c = 0; c < 10; ++c) {
+        fscanf(fp, "%d", &conv2_b[c]);
+    }
+    printf("conv2_b load succeed\n");
+    fclose(fp);
+
+
+    if (NULL == (fp = fopen("..\\data\\conv3_w.txt", "r"))) {
+        printf("conv3_w open failed\n");
+        exit(1);
+    }
+    printf("conv3_w open succeed\n");
+    // conv3.shape = [10,1,1]
+    for (c = 0; c < 10; ++c) {
+        fscanf(fp, "%d", &conv3_w[c]);
+    }
+    printf("conv3_w load succeed\n");
+    fclose(fp);
+
+    if (NULL == (fp = fopen("..\\data\\conv3_b.txt", "r"))) {
+        printf("conv3_b open failed\n");
+        exit(1);
+    }
+    printf("conv3_b open succeed\n");
+    // conv3.b.shape = [10,1]
+    for (c = 0; c < 10; ++c) {
+        fscanf(fp, "%d", &conv2_b[c]);
+    }
+    printf("conv3_b load succeed\n");
+    fclose(fp);
+}
+
+void get_dataset() { // 'NHW'format
+    FILE * fp;
+    int n, h, w;
+    if (NULL == (fp = fopen("..\\data\\data.txt", "r"))) {
+        printf("data open failed\n");
+        exit(1);
+    }
+    for (n = 0; n < COUNT_OF_DATA; ++n) {
+        for (h = 0; h < 28; ++h) {
+            for (w = 0; w < 28; ++w) {
+                fscanf(fp, "%d", &dataset[n][h][w]);
+            }
+        }
+    }
+    fclose(fp);
+}
+
+void get_labels() {
+    FILE * fp;
+    int i;
+    if (NULL == (fp = fopen("..\\data\\label.txt", "r"))) {
+        printf("label open failed\n");
+        exit(1);
+    }
+    for (i = 0; i < COUNT_OF_DATA; i++) {
+        fscanf(fp, "%d", &labels[i]);
+    }
+    fclose(fp);
+}
+
+void model_init() {
+    get_weights();
+    get_dataset();
+    get_labels();
+}
+
+int main() {
+    int label;
+    int n, h, w;
+    int right = 0;
+    float acc;
+    printf("weights always multi %d\n", MULTI);
+    model_init();
+    for (n = 0; n < COUNT_OF_DATA; ++n) {
+        for (h = 0; h < 28; ++h) {
+            for (w = 0; w < 28; ++w) {
+                input[h][w] = dataset[n][h][w];
+            }
+        }
+        label = labels[n];
+        model_run();
+        printf("predict is %d, label is %d, cnt is %d\n", output, label, n);
+        if (output == label) {
+            right++;
+        }
+    }
+    acc = (float) right * 100 / (float) n;
+    printf("dataset evaluation finished, acc is %.2f%%\n", acc);
+    return 0;
+}
